734d063fe1b16feb219065ca7bf4f9abcd4f666d
Damitha Fernando <damitha@outlook.com>
2022-12-08 15:27:45 -0800

    Initial commit

---
+++ library.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Damitha Fernando <damitha.fernando@outlook.com>
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+#include "library.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ library.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Damitha Fernando <damitha.fernando@outlook.com>
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+#ifndef PKTFLTR_LIBRARY_H
+#define PKTFLTR_LIBRARY_H
+
+void hello();
+
+#endif //PKTFLTR_LIBRARY_H
