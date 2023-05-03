d51503a75967a2f0bead7775d4fb94928431340d
Damitha Fernando <damitha.fernando@outlook.com>
2023-05-03 12:42:22 -0700

    Add Google Testing Infra
    Add google test infra and a coupld of dummy tests to verify
    if testing is working

---
+++ tests/dummy_tests.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2023 Damitha Fernando <damitha.fernando@outlook.com>
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+// Some dummy tests to check if gtest is working
+
+#include <gtest/gtest.h>
+
+TEST(DummyTests, SomeTestThatPass) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(DummyTests, SomeOtherTestThatFail) {
+    EXPECT_EQ(2,3);
+}
