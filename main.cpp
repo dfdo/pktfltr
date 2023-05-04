e8c4477dea8bf7d3dffce3a805660d82e4fb8107
Damitha Fernando <damitha.fernando@outlook.com>
2023-05-04 10:23:09 -0700

    Add usage of libpktfltr in pktfltr executable

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
-// Copyright (c) 2022 Damitha Fernando <damitha.fernando@outlook.com>
+// Copyright (c) 2023 Damitha Fernando <damitha.fernando@outlook.com>
 //
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
 #include <iostream>
+#include "expr_parser.h" // TEMP: header from libpktfltr can be used here
 
 int main() {
+    // TEMP: to check if dependencies from libpktfltr can be used
+    // in the executable successfully
+    ExpressionParser parser;
+
     std::cout << "Hello Cruel World :(" << std::endl;
     return 0;
 }
