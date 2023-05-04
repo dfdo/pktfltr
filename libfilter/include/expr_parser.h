07aea081505d59af6df37a6edef362a54aa599d3
Damitha Fernando <damitha.fernando@outlook.com>
2023-05-04 02:14:31 -0700

    Add libpktfltr Library
    Added the library and expr_parser.cpp and expr_parser.h as first
    source files of the library.

---
+++ libfilter/include/expr_parser.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2023 Damitha Fernando <damitha.fernando@outlook.com>
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+// Parse filter expression such as "ip.src == 1.2.3.0/24 && tcp" and build
+// filter object.
+
+#ifndef PKTFLTR_EXPR_PARSER_H
+#define PKTFLTR_EXPR_PARSER_H
+
+class ExpressionParser {
+
+};
+
+#endif //PKTFLTR_EXPR_PARSER_H
---
+++ libfilter/src/expr_parser.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Damitha Fernando <damitha.fernando@outlook.com>
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+// Parse filter expression such as "ip.src == 1.2.3.0/24 && tcp" and build
+// filter object.
+
+#include "expr_parser.h"
+
+void testFunction() {
+}
